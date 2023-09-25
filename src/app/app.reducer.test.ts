bef7005d6d48add46cb15efb5b8dc9328ea012a2
ILIABARABANOV <ilianardan@gmail.com>
2023-09-26 02:06:43 +0300

    -actual update

---
+++ src/app/app.reducer.test.ts
@@ -0,0 +1,18 @@
+import { appActions, AppInitialStateType, appReducer } from "app/app.reducer";
+
+let startState: AppInitialStateType;
+
+beforeEach(() => {
+  startState = {
+    error: null,
+    status: "idle",
+    isInitialized: false,
+  };
+});
+
+test("correct error message should be set", () => {
+  const endState = appReducer(startState, appActions.setAppError({ error: "some error" }));
+  expect(endState.error).toBe("some error");
+});
+
+
